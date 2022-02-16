7fc0242fe3a225dac5d5147ce183c249b561aa92
Aditya Sharan <adityasharan@Adityas-MacBook-Air.local>
2022-02-16 20:11:31 +0530

    Added Navbarfile

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='logo'>
+            <h2> BEACHES</h2>
+            </div>
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>Destinations</li>
+            <li>Travel</li>
+            <li>Book</li>
+            <li>Views</li>
+          </ul>
+          <div className="nav-icons">
+            
+          </div>
+    </div>
+  )
+}
+
+export default Navbar
