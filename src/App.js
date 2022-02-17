a83dfaac1d9b02b793fd10f1f825e2ae0cfb591c
Aditya Sharan <adityasharan@Adityas-MacBook-Air.local>
2022-02-17 17:33:05 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
-
+import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
 function App() {
   return (
     <div >
-      Hello!!
+      <Navbar />
+      <Hero/>
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { BaSearch, BiSearch } from "react-icons/bi"
+import { BsPerson, BSPerson } from "react-icons/bs"
+import { HiOutlineMenuAlt4 } from "react-icons/hi"
+import {AiOutlineClose} from "react-icons/ai"
+import {FaFacebook,FaInstagram,FaPinterest,FaTwitter,FaYoutube} from "react-icons/fa"
+import './NavbarStyle.css'
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => setNav(!nav);
   return (
     <div className='navbar'>
-        <div className='logo'>
+      <div className={!nav ? 'logo' : 'logo dark'}>
             <h2> BEACHES</h2>
             </div>
         <ul className="nav-menu">
@@ -14,8 +21,37 @@ const Navbar = () => {
             <li>Views</li>
           </ul>
           <div className="nav-icons">
-            
+              <BiSearch className="icon"/>
+              <BsPerson className="icon"/>
           </div>
+          <div className="hamburger" onClick={handleNav}>
+        {!nav ? (<HiOutlineMenuAlt4 className='icon' />) : (<AiOutlineClose className='icon' />)}
+          </div>
+
+      <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
+        <ul className='mobile-nav'>
+                <li>Home</li>
+                <li>Destinations</li>
+                <li>Travel</li>
+                <li>Book</li>
+                <li>Views</li>
+              </ul>
+              <div className='mobile-menu-bottom'>
+                  <div className='menu-icons'>
+                    <button>Search </button>
+                    <button> Account </button>
+                  </div>
+                  <div className='social-icons'>
+                    <FaFacebook className='icon'/>
+                    <FaInstagram className='icon'/>
+                    <FaTwitter className='icon'/>
+                    <FaPinterest className='icon'/>
+                    <FaYoutube className='icon' />
+                  </div>
+              </div>
+          </div>
+
+
     </div>
   )
 }
